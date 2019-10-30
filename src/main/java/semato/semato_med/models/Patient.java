dda3d3099260a8d89e99b316eaf2161b3cc7e51c
MrWonsik <mrt.wonsik@gmail.com>
2019-10-30 16:52:36 +0100

    add patient model

--- src/main/java/semato/semato_med/models/Patient.java
+++ src/main/java/semato/semato_med/models/Patient.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Admin {
+public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -20,4 +21,17 @@ public class Admin {
     @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+
+    private String pesel;
+
+    private LocalDate birthDate;
+
+    private String postalCode;
+
+    private String city;
+
+    private String street;
+
+    private String houseNumber;
+    
 }
