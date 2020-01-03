ba7031c44c745b5c935e74b59e413c4e0c91a485
MrWonsik <mrt.wonsik@gmail.com>
2020-01-03 13:40:17 +0100

    some refactoring in clinic management

---
+++ src/main/java/semato/semato_med/controller/ClinicManagementController.java
@@ -0,0 +1,77 @@
+package semato.semato_med.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+import semato.semato_med.model.Clinic;
+import semato.semato_med.payload.ApiResponse;
+import semato.semato_med.payload.clinicMgmt.ClinicResponse;
+import semato.semato_med.payload.clinicMgmt.ClinicUpdateRequest;
+import semato.semato_med.payload.clinicMgmt.ClinicAddingRequest;
+import semato.semato_med.repository.ClinicRepository;
+import semato.semato_med.service.ClinicManagementService;
+
+import java.util.Optional;
+
+
+@RestController
+@Secured({"ROLE_ADMIN"})
+@RequestMapping("/api/mgmt/clinic")
+public class ClinicManagementController {
+
+    private final ResponseEntity notFoundResponse = new ResponseEntity<>(new ApiResponse(false, "Clinic not found!"), HttpStatus.NOT_FOUND);
+
+    @Autowired
+    private ClinicManagementService clinicService;
+
+    @Autowired
+    private ClinicRepository clinicRepository;
+
+    @PutMapping("/add")
+    public ResponseEntity<?> addClinic(@RequestBody ClinicAddingRequest clinicAddingRequest) {
+        Optional<Clinic> clinicOptional = clinicRepository.findByEmail(clinicAddingRequest.getEmail());
+        if (!clinicOptional.isPresent()) {
+            return new ResponseEntity<>(new ApiResponse(false, "This clinic already exists!"), HttpStatus.BAD_REQUEST);
+        }
+
+        clinicService.add(clinicAddingRequest);
+        return new ResponseEntity<>(new ApiResponse(true, "Clinic has been added!"), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/{clinicId}")
+    public ResponseEntity<?> getClinicById(@PathVariable Long clinicId) {
+        Optional<Clinic> clinicOptional = clinicRepository.findById(clinicId);
+        return clinicOptional.map(clinic -> new ResponseEntity<>(new ClinicResponse(clinic), HttpStatus.OK)).orElse(notFoundResponse);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAllClinics() {
+        return new ResponseEntity<>(clinicService.getAll(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/del/{clinicId}")
+    public ResponseEntity<?> deleteClinicById(@PathVariable Long clinicId) {
+        Optional<Clinic> clinicOptional = clinicRepository.findById(clinicId);
+        if (!clinicOptional.isPresent()) {
+            return notFoundResponse;
+        }
+        clinicService.delete(clinicOptional.get());
+        return new ResponseEntity<>(new ApiResponse(true, "Clinic deleted successfully"), HttpStatus.OK);
+}
+
+    @PostMapping("/update/{clinicId}")
+    public ResponseEntity<?> updateNotification(
+            @PathVariable Long clinicId,
+            @RequestBody ClinicUpdateRequest clinicUpdateRequest) {
+
+        Optional<Clinic> clinicOptional = clinicRepository.findById(clinicId);
+        if (!clinicOptional.isPresent()) {
+            return notFoundResponse;
+        }
+
+        clinicService.update(clinicOptional.get(), clinicUpdateRequest);
+        return new ResponseEntity<>(new ApiResponse(true, "Clinic updated successfully"), HttpStatus.OK);
+    }
+}
--- src/main/java/semato/semato_med/service/ClinicManagementService.java
+++ src/main/java/semato/semato_med/service/ClinicManagementService.java
@@ -17,96 +17,52 @@ import java.util.Optional;
 public class ClinicManagementService {
 
     @Autowired
-    ClinicRepository clinicRepository;
+    private ClinicRepository clinicRepository;
 
-    public Clinic add(ClinicAddingRequest clinicAddingRequest){
-        Optional<Clinic> clinicOptional = clinicRepository.findByEmail(clinicAddingRequest.getEmail());
-        if(!clinicOptional.isPresent()) {
-            Clinic clinic = new Clinic(
-                    clinicAddingRequest.getName(),
-                    clinicAddingRequest.getCity(),
-                    clinicAddingRequest.getCountry(),
-                    clinicAddingRequest.getPostalCode(),
-                    clinicAddingRequest.getStreet(),
-                    clinicAddingRequest.getHouseNumber(),
-                    clinicAddingRequest.getEmail(),
-                    clinicAddingRequest.getLatitude(),
-                    clinicAddingRequest.getLongitude(),
-                    clinicAddingRequest.getImageUrl());
-            clinicRepository.save(clinic);
-            return clinic;
-        }
-
-        return null;
+    public void add(ClinicAddingRequest clinicAddingRequest) {
+        Clinic clinic = new Clinic(
+                clinicAddingRequest.getName(),
+                clinicAddingRequest.getCity(),
+                clinicAddingRequest.getCountry(),
+                clinicAddingRequest.getPostalCode(),
+                clinicAddingRequest.getStreet(),
+                clinicAddingRequest.getHouseNumber(),
+                clinicAddingRequest.getEmail(),
+                clinicAddingRequest.getLatitude(),
+                clinicAddingRequest.getLongitude(),
+                clinicAddingRequest.getImageUrl());
+        clinicRepository.save(clinic);
     }
 
-    public ClinicResponse get(Long id){
-        Optional<Clinic> clinicOptional = clinicRepository.findById(id);
-        return clinicOptional.map(clinic -> new ClinicResponse(clinic.getId(),
-                clinic.getName(),
-                clinic.getCity(),
-                clinic.getCountry(),
-                clinic.getPostalCode(),
-                clinic.getStreet(),
-                clinic.getHouseNumber(),
-                clinic.getEmail(),
-                clinic.getLatitude(),
-                clinic.getLongitude(),
-                clinic.getImageUrl(),
-                clinic.getDeletedAt())).orElse(null);
-
-    }
-
-    public List<ClinicResponse> getAll(){
+    public List<ClinicResponse> getAll() {
         List<Clinic> clinicList = clinicRepository.findAll();
         List<ClinicResponse> clinicResponses = new ArrayList<>();
         for (Clinic clinic : clinicList) {
-            clinicResponses.add(new ClinicResponse(
-                    clinic.getId(),
-                    clinic.getName(),
-                    clinic.getCity(),
-                    clinic.getCountry(),
-                    clinic.getPostalCode(),
-                    clinic.getStreet(),
-                    clinic.getHouseNumber(),
-                    clinic.getEmail(),
-                    clinic.getLatitude(),
-                    clinic.getLongitude(),
-                    clinic.getImageUrl(),
-                    clinic.getDeletedAt()));
+            clinicResponses.add(new ClinicResponse(clinic));
         }
 
         return clinicResponses;
     }
 
-    public Clinic update(ClinicUpdateRequest clinicUpdateRequest, Long clinicId){
-        Optional<Clinic> clinic = clinicRepository.findById(clinicId);
-        if(clinic.isPresent()){
-            Optional.ofNullable(clinicUpdateRequest.getName()).ifPresent(clinic.get()::setName);
-            Optional.ofNullable(clinicUpdateRequest.getCity()).ifPresent(clinic.get()::setCity);
-            Optional.ofNullable(clinicUpdateRequest.getCountry()).ifPresent(clinic.get()::setCountry);
-            Optional.ofNullable(clinicUpdateRequest.getPostalCode()).ifPresent(clinic.get()::setPostalCode);
-            Optional.ofNullable(clinicUpdateRequest.getStreet()).ifPresent(clinic.get()::setStreet);
-            Optional.ofNullable(clinicUpdateRequest.getHouseNumber()).ifPresent(clinic.get()::setHouseNumber);
-            Optional.ofNullable(clinicUpdateRequest.getEmail()).ifPresent(clinic.get()::setEmail);
-            Optional.ofNullable(clinicUpdateRequest.getLatitude()).ifPresent(clinic.get()::setLatitude);
-            Optional.ofNullable(clinicUpdateRequest.getLongitude()).ifPresent(clinic.get()::setLongitude);
-            Optional.ofNullable(clinicUpdateRequest.getImageUrl()).ifPresent(clinic.get()::setImageUrl);
-            clinicRepository.save(clinic.get());
-            return clinic.get();
-        }
+    public void update(Clinic clinic, ClinicUpdateRequest clinicUpdateRequest) {
 
-        return null;
+        Optional.ofNullable(clinicUpdateRequest.getName()).ifPresent(clinic::setName);
+        Optional.ofNullable(clinicUpdateRequest.getCity()).ifPresent(clinic::setCity);
+        Optional.ofNullable(clinicUpdateRequest.getCountry()).ifPresent(clinic::setCountry);
+        Optional.ofNullable(clinicUpdateRequest.getPostalCode()).ifPresent(clinic::setPostalCode);
+        Optional.ofNullable(clinicUpdateRequest.getStreet()).ifPresent(clinic::setStreet);
+        Optional.ofNullable(clinicUpdateRequest.getHouseNumber()).ifPresent(clinic::setHouseNumber);
+        Optional.ofNullable(clinicUpdateRequest.getEmail()).ifPresent(clinic::setEmail);
+        Optional.ofNullable(clinicUpdateRequest.getLatitude()).ifPresent(clinic::setLatitude);
+        Optional.ofNullable(clinicUpdateRequest.getLongitude()).ifPresent(clinic::setLongitude);
+        Optional.ofNullable(clinicUpdateRequest.getImageUrl()).ifPresent(clinic::setImageUrl);
+        clinicRepository.save(clinic);
+
     }
 
-    public boolean delete(Long clinicId){
-        Optional<Clinic> clinic = clinicRepository.findById(clinicId);
-        if (clinic.isPresent()) {
-            clinic.get().setDeletedAt(LocalDateTime.now());
-            clinicRepository.save(clinic.get());
-            return true;
-        }
+    public void delete(Clinic clinic) {
+        clinic.setDeletedAt(LocalDateTime.now());
+        clinicRepository.save(clinic);
 
-        return false;
     }
 }
